ca17a633fa90f844618ae1eed064e1ac695179c6
Judd Maltin <judd@newgoliath.com>
2024-08-12 16:18:54 -0400

    some class stuff

--- src/purpose_signup.py
+++ src/purpose_signup.py
@@ -11,6 +11,7 @@ def main_signup():
         workshop_choice = get_workshop_choice()
         print("You chose workshop ID number ", workshop_choice)
         signup_info = get_signup_info(workshop_choice)
+        print("Sign-up Info:", signup_info)
         confirm = input("Do you want to confirm your sign up? Press Y to confirm or N to cancel: ")
         if confirm == "Y" or confirm == "y":
             save_signup(signup_info)
@@ -30,9 +31,26 @@ def save_options_json(options):
     # write you code to write to the file "workshop_signups.json"
     # here
 
+
 def read_options_json(file_name):
     file_object = open(file_name, "r")
     return json.load(file_object)
+
+def save_signup(signup_info):
+    print("In Save Signup!")
+    print("Data I want to save:", signup_info)
+    # call the other function to save this data
+    # include, as json, the person's name, email, and the workshop ID
+    with open('data.json', 'a') as f:
+      json.dump(signup_info, f, ensure_ascii=False)
+
+
+    #file_object = open("data.json",'w', encoding='utf-8')
+    #json.dump(signup_info, file_object, ensure_ascii=False, indent=4)
+
+    return
+
+
 
 def get_workshop_choice():
     print("Which workshop do you want to sign up for?")
@@ -48,14 +66,13 @@ def get_workshop_choice():
 def get_signup_info(workshop_choice):
     answer = {}
     print("What is your name?")
+
     answer["name"] = input()
+    answer["workshop"] = workshop_choice
+
     print("You told me:")
     print(answer["name"])
     return answer
 
-def save_signup(signup_info):
-    # call the other function to save this data
-    # include, as json, the person's name, email, and the workshop ID
-    return
 
 main_signup()
